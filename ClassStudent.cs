c77a7e8d1b11d306ddf826072ca85b14cabce402
Tryals <killerzo@yandex.com>
2023-04-05 09:53:19 +0300

    Добавьте файлы проекта.

---
+++ ClassStudent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppClassFirst
+{
+    internal class ClassStudent
+    {
+        //поля
+        private string fio;
+        private string group;
+        private string progress;
+        // свойства для доступа к полям
+        public string FIO
+        {
+            get { return fio; }//возвращает значение поля
+            set { fio = value; } //устанавливает значение поля
+        }
+               
+        public string PROGRESS
+        {
+            get { return progress; }
+            set { progress = value; }
+        }
+        public string GROUP
+        {
+            get { return group; }
+            set { group = value; }
+        }
+        
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClassStudent()
+        {
+            fio = "";
+            group = "";
+            progress = "";
+        }
+        //конструктор с параметрами
+        public ClassStudent(string f, string g, string p)
+        {
+            fio = f;
+            group = g;
+            progress = p;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+           string line = $"Студент {fio}" +"\n";
+           line += $"Учится в группе {group}"+"\n";
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Личные данные студента");
+            streamWriter.WriteLine($"ФИО: {fio}");
+            streamWriter.WriteLine($"Группа: {group}");
+            streamWriter.Close();
+        }
+   
+       
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppClassFirst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    { 
+        ClassStudent student = new ClassStudent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSavestudent_Click(object sender, RoutedEventArgs e)
+        {
+            student.FIO = TxtN.Text;
+            student.GROUP = TxtGroup.Text;
+            student.PROGRESS = TxtProg.Text;
+
+            LstStudents.Items.Clear();
+            LstStudents.Items.Add(student.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            student.SaveToFile(saveFileDialog.FileName);
+        }
+    }
+}
